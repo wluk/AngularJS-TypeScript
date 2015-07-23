947fc1db45e9c519cd08510d8dec9b324ab2bd77
wluk <wlukasz91@hotmail.com>
2015-07-23 08:38:52 +0200

    PizzaHouse TypeScript ćwiczenie

---
+++ app/reference.ts
@@ -0,0 +1,10 @@
+/// <reference path="services/services.ts" />
+
+//grunt-start
+/// <reference path="controllers/MainController.ts" />
+/// <reference path="services/PizzaService.ts" />
+/// <reference path="vendor.d.ts" />
+//grunt-end
+
+/// <reference path="controllers/controllers.ts" />
+/// <reference path="main.ts" />
