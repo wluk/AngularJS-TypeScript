7067ace38324e2fa4efa07a11e42fc085e15fecd
wluk <wlukasz91@hotmail.com>
2015-07-23 08:43:32 +0200

    ToDoList ćwiczenie

--- app/reference.ts
+++ app/reference.ts
@@ -1,10 +1,5 @@
-/// <reference path="services/services.ts" />
-
 //grunt-start
-/// <reference path="controllers/MainController.ts" />
-/// <reference path="services/PizzaService.ts" />
 /// <reference path="vendor.d.ts" />
 //grunt-end
 
-/// <reference path="controllers/controllers.ts" />
 /// <reference path="main.ts" />
